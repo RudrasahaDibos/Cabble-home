e9223c2d92dd1a254345d6d4bf7d345130b01a7c
Rudra saha Dibos <96840518+RudrasahaDibos@users.noreply.github.com>
2025-01-19 19:39:36 +0600

    add password and confrim pasword match

--- src/Component/LoginandRegister/Register.jsx
+++ src/Component/LoginandRegister/Register.jsx
@@ -3,48 +3,55 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../AuthProvider/AuthProvider";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
-import { IoEye,IoEyeOff} from "react-icons/io5";
+import { IoEye, IoEyeOff } from "react-icons/io5";
 
 
 
 
 const Register = () => {
-    const {Createuser} = useContext(AuthContext)
-    const [password ,setpassword] = useState(false)
-    const [confirmpassword , setconfirmpassword] = useState(false)
-     const handlepasswordClick =()=>{
-        setpassword(!password)
-     }
-     const handleconfirpassword =()=>{
+    const { Createuser } = useContext(AuthContext)
+    const [passwordeye, setpassword] = useState(false)
+    const [confirmpassword, setconfirmpassword] = useState(false)
+    const handlepasswordClick = () => {
+        setpassword(!passwordeye)
+    }
+    const handleconfirpassword = () => {
         setconfirmpassword(!confirmpassword)
-     }
-
-        const {
-          register,
-          handleSubmit,
-          formState: { errors },
-        } = useForm({
-            mode:"onTouched"
-        })
-    
-        const onSubmit = (data) =>{
-        const {email,password} = data
-         
+    }
 
 
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm({
+        mode: "onTouched"
+    })
+              // checked password 
+
+             const password = watch("password");
+                console.log(password)
+
+    const onSubmit = (data) => {
+        const { email, password } = data
 
 
-            Createuser(email,password)
-            .then(result =>{
+
+
+
+        Createuser(email, password)
+            .then(result => {
                 console.log(result.user)
                 console.log(toast.success('Successfully login'))
-              
+
             })
-            .catch(error =>{
+            .catch(error => {
                 console.log(error)
-               console.log( toast.error(error))
+                console.log(toast.error(error))
             })
-        }
+    }
+ 
 
     return (
         <div>
@@ -54,79 +61,89 @@ const Register = () => {
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                 Sign Up
+                                Sign Up
                             </h1>
-                            <form onSubmit={handleSubmit(onSubmit)}  className="space-y-4 md:space-y-6" action="#">
+                            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 md:space-y-6" action="#">
                                 <div>
                                     <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
-                                    <input type="text" name="name" id="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" 
-                                    
-                                    {...register("name")}
-                                    {...register("name", { required: true })}
+                                    <input type="text" name="name" id="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"
+
+                                        {...register("name")}
+                                        {...register("name", { required: true })}
                                     />
-                                  {errors.name && <span className="text-red-800">This field is required</span>}
+                                    {errors.name && <span className="text-red-800">This field is required</span>}
                                 </div>
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" 
-                                    
-                                    {...register("email")}
-                                    {...register("email", { required: true })}
+                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"
+
+                                        {...register("email")}
+                                        {...register("email", { required: true })}
                                     />
-                                  {errors.email && <span className="text-red-800">This field is required</span>}
+                                    {errors.email && <span className="text-red-800">This field is required</span>}
                                 </div>
                                 <div className="relative">
                                     <label htmlFor="password" className={`block mb-2 text-sm font-medium text-white`}>Password</label>
-                                    <input type={password ? "password"  : "text"}  name="password" id="password" placeholder="••••••••" className={`bg-gray-50 border ${errors.password && "bg-red-700"} border-gray-300 text-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
-                                     {...register("password")}
-                                     {...register("password", { required: "password is Required",
-                                        pattern:{
-                                           value:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                                           message:"Must include at least one uppercase letter, one lowercase letter, and one number May include special characters (!@#$%^&*)"
-                                        },
-                                        minLength:{
-                                            value:8,
-                                            message:'minimum Required length is 8'
-                                        },
-                                        maxLength:{
-                                            value:20,
-                                            message:"maximum length is 20"
+                                    <input type={passwordeye ? "password" : "text"} name="password" id="password" placeholder="••••••••" className={`bg-gray-50 border ${errors.password && "bg-red-700"} border-gray-300 text-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                                        {...register("password")}
+                                        {...register("password", {
+                                            required: "password is Required",
+                                            // pattern: {
+                                            //     value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                                            //     message: "Must include at least one uppercase letter, one lowercase letter, and one number May include special characters (!@#$%^&*)"
+                                            // },
+                                            minLength: {
+                                                value: 8,
+                                                message: 'minimum Required length is 8'
+                                            },
+                                            maxLength: {
+                                                value: 20,
+                                                message: "maximum length is 20"
+                                            }
+
+                                        })}
+                                    />
+                                    {errors.password && <span className="text-red-600">{errors.password.message}</span>}
+                                    <div className="text-2xl  absolute text-black top-9 right-5 ">
+                                        {
+                                            (passwordeye === false) ? <IoEye onClick={handlepasswordClick} /> : <IoEyeOff onClick={handlepasswordClick} />
                                         }
 
-                                      })}
-                                    />
-                                     {errors.password && <span className="text-red-600">{errors.password.message}</span>}
-                                     <div className="text-2xl  absolute text-white top-9 right-5 "> 
-                                        {
-                                            (password === false) ? <IoEye onClick={handlepasswordClick} />: <IoEyeOff   onClick={handlepasswordClick}  /> 
-                                        }
-                                     
-                                      </div>
+                                    </div>
                                 </div>
                                 <div className="relative">
                                     <label htmlFor="confirm_password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                    <input type={confirmpassword ? 'password':'text'}name="confirm_password" id="confirm_password" placeholder="••••••••" className="bg-gray-50 border  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                                     {...register("confirm_password")}
-                                     {...register("confirm_password", { required: true ,
-                                        
+                                    <input type={confirmpassword ? 'password' : 'text'}
+                                
+                                    name="confirmpassword" id="confirmpassword" placeholder="••••••••" className={`bg-gray-50 border ${errors.confirmpassword && "bg-red-700"} border-gray-300 text-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                                        {...register("confirmpassword")}
+                                        {...register("confirmpassword", {
+                                            required: "confirmpassword is Required",
+                                           validate:(value)=>{
+                                           
+                                           if(value !== password){
+                                            return 'This password not match'
+                                           }
+                                             
+                                           }
 
-                                      })}
-                                    
+                                        })}
+
                                     />
-                                       {errors.confirm_password && <span>This field is required</span>}
-                                      <div className="text-2xl  absolute text-white top-9 right-5 "> 
+                                    {errors.confirmpassword && <span className="text-red-700">{errors.confirmpassword.message}</span>}
+                                    <div className="text-2xl  absolute text-white top-9 right-5 ">
                                         {
-                                            (confirmpassword === false) ? <IoEye onClick={handleconfirpassword } />: <IoEyeOff   onClick={handleconfirpassword }  /> 
+                                            (confirmpassword === false) ? <IoEye onClick={handleconfirpassword} /> : <IoEyeOff onClick={handleconfirpassword} />
                                         }
-                                     
-                                      </div> 
+
+                                    </div>
                                 </div>
                                 <div className="flex items-start">
                                     <div className="flex items-center h-5">
-                                        <input id="terms" aria-describedby="terms" name="checked" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" 
-                                         {...register("checked")}
-                                         {...register("checked", { required: true  })}
-                                        
+                                        <input id="terms" aria-describedby="terms" name="checked" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                            {...register("checked")}
+                                            {...register("checked", { required: true })}
+
                                         />
                                         {errors.checked && <span>This field is required</span>}
                                     </div>
